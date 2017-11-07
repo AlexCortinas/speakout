abd042e91e94f1af33381f8f7637cc4bcf12291c
Álex Cortiñas <mail@alexcortinas.es>
2017-11-07 10:36:35 +0100

    Adding english translations for labels

--- includes/emailpetition.php
+++ includes/emailpetition.php
@@ -86,12 +86,12 @@ function dk_speakout_emailpetition_shortcode( $attr ) {
 							</div>
 							
 							<div class="dk-speakout-full">
-								<label for="dk-speakout-first-name-' . $petition->id . '" class="required">' . __( 'First Name', 'speakout' ) . '</label>
+								<label for="dk-speakout-first-name-' . $petition->id . '" class="required">' . __( 'First Name', 'speakout' ) . '<span class="translation"> First Name</span></label>
 								<input name="dk-speakout-first-name" id="dk-speakout-first-name-' . $petition->id . '" value="' . $userdata['firstname'] . '" type="text" />
 							</div>
 							
 							<div class="dk-speakout-full">
-								<label for="dk-speakout-last-name-' . $petition->id . '" class="required">' . __( 'Last Name', 'speakout' ) . '</label>
+								<label for="dk-speakout-last-name-' . $petition->id . '" class="required">' . __( 'Last Name', 'speakout' ) . '<span class="translation"> Last Name</span></label>
 								<input name="dk-speakout-last-name" id="dk-speakout-last-name-' . $petition->id . '" value="' . $userdata['lastname'] . '" type="text" />
 							</div>
 							<div class="dk-speakout-full">
@@ -101,14 +101,14 @@ function dk_speakout_emailpetition_shortcode( $attr ) {
 				if ( $petition->requires_confirmation ) {
 					$petition_form .= '
 							<div class="dk-speakout-full">
-								<label for="dk-speakout-email-confirm-' . $petition->id . '" class="required">' . __( 'Confirm Email', 'speakout' ) . '</label>
+								<label for="dk-speakout-email-confirm-' . $petition->id . '" class="required">' . __( 'Confirm Email', 'speakout' ) . '<span class="translation"> Email</span></label>
 								<input name="dk-speakout-email-confirm" id="dk-speakout-email-confirm-' . $petition->id . '" value="" type="text" />
 							</div>';
 				}
 				if ( in_array( 'street', $petition->address_fields ) ) {
 					$petition_form .= '
 							<div class="dk-speakout-full">
-								<label for="dk-speakout-street-' . $petition->id . '">' . __( 'Street', 'speakout' ) . '</label>
+								<label for="dk-speakout-street-' . $petition->id . '">' . __( 'Street', 'speakout' ) . '<span class="translation"> Organization / Profession</span></label>
 								<input name="dk-speakout-street" id="dk-speakout-street-' . $petition->id . '" maxlength="200" type="text" />
 							</div>';
 				}
@@ -116,7 +116,7 @@ function dk_speakout_emailpetition_shortcode( $attr ) {
 				if ( in_array( 'city', $petition->address_fields ) ) {
 					$petition_form .= '
 							<div class="dk-speakout-half">
-								<label for="dk-speakout-city-' . $petition->id . '">' . __( 'City', 'speakout' ) . '</label>
+								<label for="dk-speakout-city-' . $petition->id . '">' . __( 'City', 'speakout' ) . '<span class="translation"> City</span></label>
 								<input name="dk-speakout-city" id="dk-speakout-city-' . $petition->id . '" maxlength="200" type="text" />
 							</div>';
 				}
@@ -138,7 +138,7 @@ function dk_speakout_emailpetition_shortcode( $attr ) {
 				if ( in_array( 'country', $petition->address_fields ) ) {
 					$petition_form .= '
 							<div class="dk-speakout-half">
-								<label for="dk-speakout-country-' . $petition->id . '">' . __( 'Country', 'speakout' ) . '</label>
+								<label for="dk-speakout-country-' . $petition->id . '">' . __( 'Country', 'speakout' ) . '<span class="translation"> Country</span></label>
 								<input name="dk-speakout-country" id="dk-speakout-country-' . $petition->id . '" maxlength="200" type="text" list="dk-speakout-countries" />
 								<datalist id="dk-speakout-countries">
 									<option value="Afghanistan"><option value="Albania"><option value="Algeria"><option value="American Samoa"><option value="Andorra"><option value="Angola"><option value="Anguilla"><option value="Antarctica"><option value="Antigua and Barbuda"><option value="Argentina"><option value="Armenia"><option value="Aruba"><option value="Australia"><option value="Austria"><option value="Azerbaijan"><option value="Bahrain"><option value="Bangladesh"><option value="Barbados"><option value="Belarus"><option value="Belgium"><option value="Belize"><option value="Benin"><option value="Bermuda"><option value="Bhutan"><option value="Bolivia"><option value="Bosnia and Herzegovina"><option value="Botswana"><option value="Bouvet Island"><option value="Brazil"><option value="British Indian Ocean Territory"><option value="British Virgin Islands"><option value="Brunei"><option value="Bulgaria"><option value="Burkina Faso"><option value="Burundi"><option value="Côte d\'Ivoire"><option value="Cambodia"><option value="Cameroon"><option value="Canada"><option value="Cape Verde"><option value="Cayman Islands"><option value="Central African Republic"><option value="Chad"><option value="Chile"><option value="China"><option value="Christmas Island"><option value="Cocos (Keeling) Islands"><option value="Colombia"><option value="Comoros"><option value="Congo"><option value="Cook Islands"><option value="Costa Rica"><option value="Croatia"><option value="Cuba"><option value="Cyprus"><option value="Czech Republic"><option value="Democratic Republic of the Congo"><option value="Denmark"><option value="Djibouti"><option value="Dominica"><option value="Dominican Republic"><option value="East Timor"><option value="Ecuador"><option value="Egypt"><option value="El Salvador"><option value="Equatorial Guinea"><option value="Eritrea"><option value="Estonia"><option value="Ethiopia"><option value="Faeroe Islands"><option value="Falkland Islands"><option value="Fiji"><option value="Finland"><option value="Former Yugoslav Republic of Macedonia"><option value="France"><option value="French Guiana"><option value="French Polynesia"><option value="French Southern Territories"><option value="Gabon"><option value="Georgia"><option value="Germany"><option value="Ghana"><option value="Gibraltar"><option value="Greece"><option value="Greenland"><option value="Grenada"><option value="Guadeloupe"><option value="Guam"><option value="Guatemala"><option value="Guinea"><option value="Guinea-Bissau"><option value="Guyana"><option value="Haiti"><option value="Heard Island and McDonald Islands"><option value="Honduras"><option value="Hong Kong"><option value="Hungary"><option value="Iceland"><option value="India"><option value="Indonesia"><option value="Iran"><option value="Iraq"><option value="Ireland"><option value="Israel"><option value="Italy"><option value="Jamaica"><option value="Japan"><option value="Jordan"><option value="Kazakhstan"><option value="Kenya"><option value="Kiribati"><option value="Kuwait"><option value="Kyrgyzstan"><option value="Laos"><option value="Latvia"><option value="Lebanon"><option value="Lesotho"><option value="Liberia"><option value="Libya"><option value="Liechtenstein"><option value="Lithuania"><option value="Luxembourg"><option value="Macau"><option value="Madagascar"><option value="Malawi"><option value="Malaysia"><option value="Maldives"><option value="Mali"><option value="Malta"><option value="Marshall Islands"><option value="Martinique"><option value="Mauritania"><option value="Mauritius"><option value="Mayotte"><option value="Mexico"><option value="Micronesia"><option value="Moldova"><option value="Monaco"><option value="Mongolia"><option value="Montserrat"><option value="Morocco"><option value="Mozambique"><option value="Myanmar"><option value="Namibia"><option value="Nauru"><option value="Nepal"><option value="Netherlands"><option value="Netherlands Antilles"><option value="New Caledonia"><option value="New Zealand"><option value="Nicaragua"><option value="Niger"><option value="Nigeria"><option value="Niue"><option value="Norfolk Island"><option value="North Korea"><option value="Northern Marianas"><option value="Norway"><option value="Oman"><option value="Pakistan"><option value="Palau"><option value="Panama"><option value="Papua New Guinea"><option value="Paraguay"><option value="Peru"><option value="Philippines"><option value="Pitcairn Islands"><option value="Poland"><option value="Portugal"><option value="Puerto Rico"><option value="Qatar"><option value="Réunion"><option value="Romania"><option value="Russia"><option value="Rwanda"><option value="São Tomé and Príncipe"><option value="Saint Helena"><option value="Saint Kitts and Nevis"><option value="Saint Lucia"><option value="Saint Pierre and Miquelon"><option value="Saint Vincent and the Grenadines"><option value="Samoa"><option value="San Marino"><option value="Saudi Arabia"><option value="Senegal"><option value="Seychelles"><option value="Sierra Leone"><option value="Singapore"><option value="Slovakia"><option value="Slovenia"><option value="Solomon Islands"><option value="Somalia"><option value="South Africa"><option value="South Georgia and the South Sandwich Islands"><option value="South Korea"><option value="Spain"><option value="Sri Lanka"><option value="Sudan"><option value="Suriname"><option value="Svalbard and Jan Mayen"><option value="Swaziland"><option value="Sweden"><option value="Switzerland"><option value="Syria"><option value="Taiwan"><option value="Tajikistan"><option value="Tanzania"><option value="Thailand"><option value="The Bahamas"><option value="The Gambia"><option value="Togo"><option value="Tokelau"><option value="Tonga"><option value="Trinidad and Tobago"><option value="Tunisia"><option value="Turkey"><option value="Turkmenistan"><option value="Turks and Caicos Islands"><option value="Tuvalu"><option value="US Virgin Islands"><option value="Uganda"><option value="Ukraine"><option value="United Arab Emirates"><option value="United Kingdom"><option value="United States"><option value="United States Minor Outlying Islands"><option value="Uruguay"><option value="Uzbekistan"><option value="Vanuatu"><option value="Vatican City"><option value="Venezuela"><option value="Vietnam"><option value="Wallis and Futuna"><option value="Western Sahara"><option value="Yemen"><option value="Yugoslavia"><option value="Zambia"><option value="Zimbabwe">
@@ -149,7 +149,7 @@ function dk_speakout_emailpetition_shortcode( $attr ) {
 				if ( $petition->displays_custom_field == 1 ) {
 					$petition_form .= '
 							<div class="dk-speakout-full">
-								<label for="dk-speakout-custom-field-' . $petition->id . '" class="required">' . stripslashes( esc_html( $petition->custom_field_label ) ) . '</label>
+								<label for="dk-speakout-custom-field-' . $petition->id . '" class="required">' . stripslashes( esc_html( $petition->custom_field_label ) ) . '<span class="translation"> ID</span></label>
 								<input name="dk-speakout-custom-field" id="dk-speakout-custom-field-' . $petition->id . '" maxlength="400" type="text" />
 							</div>';
 				}
